a37a54ff4303df596d565140be2da2b710987ba5
sashadb <sashadbrown@gmail.com>
2022-02-10 17:55:45 +0000

    a

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1E-mFZ0W4ijlKXFGkZCcV5g2RNQyZN3EjJ_uWvPOsTAs/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
